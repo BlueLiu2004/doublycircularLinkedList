a9b906c749dd84b9f7f8b2b3597bb951b0cfd34e
BlueLiu2004 <BlueLiu2004@gmail.com>
2024-09-04 14:49:01 +0800

    let user can input the length of linked list

--- main.c
+++ main.c
@@ -2,13 +2,27 @@
 
 int main()
 {
+    // create a NULL head node for linked list
     Node *head = NULL;
+    size_t LIST_LENGTH = 0;
+
+    // random number generator seed
     srand(time(NULL));
-    for (int i = 0; i < 20; i++)
+    
+    printf("Input the length that you want: ");
+    scanf("%u", &LIST_LENGTH);
+    fflush(stdin);
+
+    // create nodes by random number
+    for (uint32_t i = 0; i < LIST_LENGTH; i++)
     {
         addNode(&head, rand());
     }
 
+    // if want to print linked list from tail, edit second argument to 1 (or any not zero integer)
     printList(head, 0);
+
+    // free the linked list to avoid memory leak
+    freeList(&head);
     return EXIT_SUCCESS;
 }
